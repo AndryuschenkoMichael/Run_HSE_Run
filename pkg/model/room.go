88547b29935cafe8fa1eccabf420d42ce7220bf6
Misha <maandryuschenko@edu.hse.ru>
2022-05-12 00:46:18 +0300

    Fix problem

--- pkg/model/room.go
+++ pkg/model/room.go
@@ -2,6 +2,6 @@ package model
 
 type Room struct {
 	Id       int    `json:"id" db:"id"`
-	Code     string `json:"code" db:"id"`
+	Code     string `json:"code" db:"code"`
 	CampusId int    `json:"campus_id" db:"campus_id"`
 }
